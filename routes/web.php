b4fd60377d9b9acca0dd4571255c01fc167d869d
Amirhamja422 <amirhamja422@gmail.com>
2022-12-26 13:30:38 +0600

    Basic Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/greeting', function () {
+//     return 'Hello World';
+// });
+
+
+// Route::get('/user/{id?}/{name?}', function ($userid=null,$userName=null) {
+//     return 'User Id is'.$userid."User Name is".$userName;
+// });
+
+
+// return er value gulo view file dekhate hole jevabe korte hoi
+
+// Route::get('/greeting', function () {
+//    return view('greeting');
+// });
+
+
+Route::get('/user/{id?}/{name?}', function ($userid=null,$userName=null) {
+    return view('user.index',
+    ['UserId'=>$userid,
+    'UserName'=>$userName,
+   ]);
 });
+
+
+//Route::get('/user', [UserController::class, 'index']);

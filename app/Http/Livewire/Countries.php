0f78e43f124bf54eaee7b32ef00c6eeb1ebb6b54
Amirhamja422 <amirhamja422@gmail.com>
2023-08-05 23:22:28 +0600

    form created

--- app/Http/Livewire/Countries.php
+++ app/Http/Livewire/Countries.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Livewire;
+use App\Models\Continent;
+use App\Models\Country;
 
 use Livewire\Component;
 
@@ -8,7 +10,10 @@ class Countries extends Component
 {
     public function render()
     {
-        return view('livewire.countries');
+        return view('livewire.countries',[
+        'continents'=>Continent::orderBy('continent_name','asc')->get(),
+        'countries'=>Country::orderBy('country_name','asc')->get()
+    ]);
     }
 
     public function OpenAddCountryModel(){
--- resources/views/models/add-model.blade.php
+++ resources/views/models/add-model.blade.php
@@ -8,8 +8,33 @@
           </button>
         </div>
         <div class="modal-body">
-          ...
-        </div>
+            <form>
+
+                <div class="mb-3">
+                  <label for="exampleInput" class="form-label">Continent</label>
+                  <select id="inputState" class="form-select" wire:model="continent">
+                    <option selected>--Select a cont--</option>
+                    @foreach ($continents as $item)
+                    <option value="{{ $item->id }}">{{ $item->continent_name }}</option>
+                    <option>test01</option>
+                    <option>test01</option>
+                    <option>test01</option>
+                    @endforeach
+                  </select>
+                </div>
+
+                <div class="mb-3">
+                  <label for="exampleInput" class="form-label">Country Name</label>
+                  <input type="text" class="form-control" id="country_name"  name="country_name" aria-describedby="emailHelp">
+                </div>
+
+                <div class="mb-3">
+                  <label for="exampleInput" class="form-label">Capital City</label>
+                  <input type="text" class="form-control" id="capital_city" name="capital_city" aria-describedby="emailHelp">
+                </div>
+
+              </form>
+            </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           <button type="button" class="btn btn-primary">Save changes</button>
